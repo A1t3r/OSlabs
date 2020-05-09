a53a31bf205d84cdb7ca3c10d47d5c70c1821d4d
anton <korkunov_a@list.ru>
2020-05-09 18:05:22 +0300

    test

--- UPDaemon.c
+++ UPDaemon.c
@@ -1,3 +1,4 @@
+#define NN 32
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -9,153 +10,136 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <string.h>
+#include <resolv.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 sem_t sem1;
 int flag = 0;
 int Dflag = 0;
 
-void sigalarm_handler(int fd){
+void sigalarm_handler(){
 flag=1;
 }
-/*
-void term_handler(int fd){
+
+void term_handler(){
 Dflag=1;
 }
-*/
+
 int Daemon(char*path[]){
     signal(SIGALRM,sigalarm_handler);
-    //signal(SIGTERM,term_handler);
+    signal(SIGTERM,term_handler);
+
+  //const char *semname="/semname";
+   sem_init(&sem1, 0, 0);
+   sem_post(&sem1);
+   //sem1 = sem_open(semname,O_CREAT);//|O_EXCL);
+  // sem_post(sem1);
+  // sem_wait(&sem1);
+    char* tmp=NULL;
+    char* newpar[NN];
+    for(int i=0;i<NN;i++){
+          newpar[i]=NULL;
+      }
+
+for(;;) {
+    if (flag == 1) {
     int fd;
     char buf[256]={};
     fd = open(path[1],O_CREAT|O_RDWR,S_IRWXU);
-   if(read(fd, buf, sizeof(buf)-1)==-1) exit(9);
-    int tmpsize = strlen(buf);
-// разбиени строки на подстроки с сохранением массива строк в динамической памяти + cоздание массива массивов строк для работы с несколькими командами
+    if(read(fd, buf, sizeof(buf)-1)==-1) exit(9);
+     close(fd);
+/*  
     int cnt=0;
     int cnnt=0;
-    int mcnt=8;
+    int mcnt=4;
     for(int i =0;i< sizeof(buf);i++) {
-        if (buf[i] == '\n') {
+       if (buf[i] == '\n'){ 
             cnt++;
             cnnt=0;
-        }
-        if (buf[i] == ' '){
+       }
+       if (buf[i] == ' '){
             cnnt++;
-            if (mcnt<cnnt) mcnt=cnnt;}
+            if (mcnt<cnnt) mcnt=cnnt;
+       }
     }
     mcnt++;
-    cnt++;
-    char** newtoexec=(char**)calloc((cnt),sizeof(char*));
-    for(int i=0;i<cnt;i++){
-        newtoexec[i]=(char*)calloc(32,sizeof(char));
-    }
-    char*** newpar=(char***)calloc((cnt),sizeof(char**));
-    for(int i=0;i<cnt;i++){
-        newpar[i]=(char**)calloc((mcnt),sizeof(char*));
-        for(int j=0;j<mcnt;j++){
-            newpar[i][j]=(char*)calloc(32,sizeof(char));
-            newpar[i][j]=NULL;
-        }
-    }
+*/
+  //=(char**)calloc((mcnt),sizeof(char*));
+ // for(int i=0;i<mcnt;i++){
+   //  newpar[i]=(char*)calloc(32,sizeof(char));
+   //  }
 
     int k=0;
     int n =0;
     int j =0;
     int frst=0;
-    newpar[n][0]="test";
-    char* tmp = (char*)calloc(32,sizeof(char));
-    for(int i =0;i< sizeof(buf)-1;i++) {
-        if (buf[i] == '\n') {
-            if (frst == 0) {
-                k++;
-                tmp[j] = '\0';
-                newtoexec[n] = tmp;
-            }
-            else {
-                tmp[j] = '\0';
-                k++;
-                newpar[n][k] = tmp;
-            }
-            tmp = (char *) calloc(32 , sizeof(char));
-            n++;
-            newpar[n][0]="test";
-            k=0;
-            j = 0;
-            frst=0;
-            continue;
-        }
-        if (buf[i] == ' ') {
-            if (frst == 1) {
-                k++;
-                tmp[j] = '\0';
-                newpar[n][k] = tmp;
-                tmp = (char *) calloc(32 , sizeof(char));
-                j = 0;
-                continue;
-            } else {
-                tmp[j] = '\0';
-                newtoexec[n] = tmp;
-                tmp = (char *) calloc(32 , sizeof(char));
-                j = 0;
-                frst = 1;
-                continue;
-            }
-        }
-        tmp[j] = buf[i];
-        j++;
-    }
-    tmp[j] = '\0';
-    if(frst==0){
-        newtoexec[n]=tmp;
-    }
-    else{
-        newpar[n][k+1]=tmp;
-        newpar[n][k+2]=NULL;
-    }
-    free(tmp);
-/////////////////////////////////////////////////////////////////////////
-    close(1);
-    dup2(fd, 1);
 
-  // const char *semname="/semname";
-   sem_init(&sem1, 0, 0);
-   sem_post(&sem1);
-  // sem1 = sem_open(semname,O_CREAT);//|O_EXCL);
-  // sem_wait(&sem1);
+   tmp = (char*)calloc(32,sizeof(char));
+   for(int i =0;i< strlen(buf);i++) {
+   if (buf[i] == ' ') {
+        tmp[j] = '\0';
+        newpar[n] = tmp;
+        tmp = (char *) calloc(32 , sizeof(char));
+        j = 0;
+        i++;
+        n++;
+       }
+   if (buf[i] == '\n' || (buf[i]=='\0')) {
+        tmp[j] = '\0';
+        newpar[n] = tmp;
+        tmp = (char *) calloc(32 , sizeof(char));
+        j = 0;
+        i++;
+        n++;
 
-
-for(;;) {
-    if (flag == 1) {
-        
-        for (int i=0;i<cnt-1;i++){
- 
-             pid_t testpid;
-             testpid = fork();
+        pid_t testpid;
+        testpid = fork();
              if(testpid==0) {
                      sem_wait(&sem1);
+                    // sem_wait(sem1);
                      int fd2 = open("UPdemLOG.txt",O_CREAT|O_RDWR,S_IRWXU);
                      lseek(fd2, 0, SEEK_END);
-                     write(fd2,"\n NOW WORKING: \n",13);
-                     write(fd2,newtoexec[i],strlen(newtoexec[i])); 
+                     write(fd2,"\n NOW WORKING:\n",14);
+                     write(fd2,newpar[0],strlen(newpar[0])); 
+                     write(fd2,"\n",1);
                      close(fd2);
+                     int fd3 = open("UPdemEXECVEOUT.txt",O_CREAT|O_RDWR,S_IRWXU);
+                     lseek(fd3, 0, SEEK_END);
+                     close(1);
+                     dup2(fd3,1);
                      sem_post(&sem1);
-                     execve(newtoexec[i], newpar[i],NULL);
+                      //sem_post(sem1);
+                     execve(newpar[0], newpar,NULL);
               }
               if(testpid > 0) {
                      flag = 0;
+                     int status=0;
+                     wait(&status);
               }
 
-          }
+      for(int i=0;i<NN;i++){
+          if(newpar[i]!=NULL) free(newpar[i]);
+          newpar[i]=NULL;
       }
-    /*
-    if(Dflag==1){
-     close(fd);
-     close(fd2);
-     sem_destroy(&sem1);
-     exit(0);
-}*/
+       n=0;
+      }
+
+   tmp[j] = buf[i];
+   j++;
+  
+ }
+ free(tmp);
+
 }
-    //close(fd);
+    if(Dflag==1){
+    //close(fd2);
+    sem_destroy(&sem1);
+    exit(0);
+    }
+
+}
+   
 }
 
 int main(int argc, char* argv[],char* env[]){
