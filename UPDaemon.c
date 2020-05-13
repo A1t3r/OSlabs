8f0963341c0c7ed052cbbd9b1129c6f0ffdc9f94
anton <korkunov_a@list.ru>
2020-05-13 18:16:42 +0300

    named sem added

--- UPDaemon.c
+++ UPDaemon.c
@@ -14,7 +14,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
-sem_t sem1;
+//sem_t* sem1;
 int flag = 0;
 int Dflag = 0;
 
@@ -30,12 +30,11 @@ int Daemon(char*path[]){
     signal(SIGALRM,sigalarm_handler);
     signal(SIGTERM,term_handler);
 
-  //const char *semname="/semname";
-   sem_init(&sem1, 0, 0);
-   sem_post(&sem1);
-   //sem1 = sem_open(semname,O_CREAT);//|O_EXCL);
-  // sem_post(sem1);
-  // sem_wait(&sem1);
+  const char *semname="/semname";
+   //sem_init(&sem1, 0, 0);
+   //sem_post(&sem1);
+   sem_t* sem1 = sem_open(semname,O_CREAT);//|O_EXCL);
+   sem_post(sem1);
     char* tmp=NULL;
     char* newpar[NN];
     for(int i=0;i<NN;i++){
@@ -49,26 +48,6 @@ for(;;) {
     fd = open(path[1],O_CREAT|O_RDWR,S_IRWXU);
     if(read(fd, buf, sizeof(buf)-1)==-1) exit(9);
      close(fd);
-/*  
-    int cnt=0;
-    int cnnt=0;
-    int mcnt=4;
-    for(int i =0;i< sizeof(buf);i++) {
-       if (buf[i] == '\n'){ 
-            cnt++;
-            cnnt=0;
-       }
-       if (buf[i] == ' '){
-            cnnt++;
-            if (mcnt<cnnt) mcnt=cnnt;
-       }
-    }
-    mcnt++;
-*/
-  //=(char**)calloc((mcnt),sizeof(char*));
- // for(int i=0;i<mcnt;i++){
-   //  newpar[i]=(char*)calloc(32,sizeof(char));
-   //  }
 
     int k=0;
     int n =0;
@@ -96,8 +75,8 @@ for(;;) {
         pid_t testpid;
         testpid = fork();
              if(testpid==0) {
-                     sem_wait(&sem1);
-                    // sem_wait(sem1);
+                   //  sem_wait(&sem1);
+                     sem_wait(sem1);
                      int fd2 = open("UPdemLOG.txt",O_CREAT|O_RDWR,S_IRWXU);
                      lseek(fd2, 0, SEEK_END);
                      write(fd2,"\n NOW WORKING:\n",14);
@@ -108,8 +87,8 @@ for(;;) {
                      lseek(fd3, 0, SEEK_END);
                      close(1);
                      dup2(fd3,1);
-                     sem_post(&sem1);
-                      //sem_post(sem1);
+                    // sem_post(&sem1);
+                     sem_post(sem1);
                      execve(newpar[0], newpar,NULL);
               }
               if(testpid > 0) {
@@ -134,7 +113,8 @@ for(;;) {
 }
     if(Dflag==1){
     //close(fd2);
-    sem_destroy(&sem1);
+    sem_unlink(semname);
+    //sem_destroy(sem1);
     exit(0);
     }
 
@@ -144,7 +124,6 @@ for(;;) {
 
 int main(int argc, char* argv[],char* env[]){
     pid_t parpid;
-   // signal(SIGALRM,sigalarm_handler);
     if((parpid=fork())<0) //--здесь мы пытаемся создать дочерний процесс главного процесса (масло масляное в прямом смысле)
     {                   //--точную копию исполняемой программы
         printf("\ncan't fork"); //--если нам по какойлибо причине это сделать не удается выходим с ошибкой.
